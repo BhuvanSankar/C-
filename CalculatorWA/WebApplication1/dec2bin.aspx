639d22280a576f29a8cadcced97985757a1ae8fb
Bhuvaneswari Thirugnanasambandham <buvan.suji@gmail.com>
2018-01-22 11:30:03 +1000

    from local repo to git

---
+++ CalculatorWA/WebApplication1/dec2bin.aspx
@@ -0,0 +1,47 @@
+﻿
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="dec2bin.aspx.cs" Inherits="WebApplication1.dec2bin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="css/dectobin.css" rel="stylesheet" type="text/css"/>
+    <title></title>
+    
+</head>
+<body>
+    <form id="form1"  runat="server">
+    <div style="margin-left: auto; margin-right: auto; text-align: center;"> 
+        <h1>
+            <asp:Label ID="Label5" runat="server" Text="BINARY CALCULATOR" style="text-align: center; font-size: x-large; background-color: #FFFF99" ></asp:Label>
+        </h1>
+        &nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" Width="100px" runat="server"></asp:TextBox>
+        <asp:DropDownList ID="DropDownList1" runat="server">
+            <asp:ListItem>SELECT</asp:ListItem>
+            <asp:ListItem>+</asp:ListItem>
+            <asp:ListItem>-</asp:ListItem>
+            <asp:ListItem>*</asp:ListItem>
+            <asp:ListItem>/</asp:ListItem>
+        </asp:DropDownList>
+        <asp:TextBox ID="TextBox2" Width="100px" runat="server"></asp:TextBox>
+        <asp:Button class="cal" ID="Button1" runat="server" Text="Calculate" OnClick="Button1_Click" />
+        <br /><br />
+        <asp:Label ID="Label1" runat="server" Text="=Base10:"></asp:Label>
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br /><br />
+        <asp:Label ID="Label2" runat="server" Text="=Base2:"></asp:Label>
+        <asp:TextBox ID="TextBox4" Width="300px" runat="server"></asp:TextBox>
+        <br /><br />
+        <asp:Button ID="Button2" Class="cal" runat="server" Text="Count" OnClick="Button2_Click" />
+        <asp:Label ID="Label3" runat="server" Text="Num of 0"></asp:Label>
+        <asp:TextBox ID="TextBox5" Width="50px" runat="server"></asp:TextBox>
+        <asp:Label ID="Label4" runat="server" Text="Num of 1"></asp:Label>
+        <asp:TextBox ID="TextBox6" Width="50px" runat="server"></asp:TextBox>
+    </div>
+    </form>
+</body>
+</html>
+
+        
---
+++ CalculatorWA/WebApplication1/dec2bin.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class dec2bin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(TextBox1.Text);
+            int y = int.Parse(TextBox2.Text);
+            int z=0;
+            if(DropDownList1.SelectedValue == "/" && y == 0)
+            {
+                TextBox3.Text = "ZERO DIVISION ERROR";
+                TextBox4.Text = "ZERO DIVISION ERROR";
+            }
+            else
+            {
+                switch (DropDownList1.SelectedValue)
+                {
+                    case "+":
+                        z = x + y;
+                        break;
+                    case "-":
+                        z = x - y;
+                        break;
+                    case "*":
+                        z = x * y;
+                        break;
+                    case "/":
+                        z = x / y;
+                        break;
+                    default:
+                        break;
+                }
+                TextBox3.Text = z.ToString();
+                int a = int.Parse(TextBox3.Text);
+                TextBox4.Text = Convert.ToString(a, 2);     
+            }
+                   
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int count0 = 0, count1 = 0;
+            int y = int.Parse(TextBox2.Text);
+            if (DropDownList1.SelectedValue == "/" && y == 0)
+            {
+                TextBox5.Text = " ";
+                TextBox6.Text = " ";
+            }
+            else
+            {
+                foreach (char s in TextBox4.Text)
+                {
+                    if (s == '0')
+                        count0++;
+                    else
+                        count1++;
+                }
+                TextBox5.Text = count0.ToString();
+                TextBox6.Text = count1.ToString();
+            }
+            
+        }
+    }
+}
